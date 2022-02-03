ec5698f66ffa6fbec2b959ff13ee6982c28e2db7
Rodin10 <r.jagga@live.nl>
2022-02-03 10:33:11 +0100

    Implemented usage of flairs on Reddit

--- reddit.py
+++ reddit.py
@@ -5,6 +5,15 @@ def init(bot_name):
     global reddit
     reddit = praw.Reddit(bot_name)
 
-def submitSelfPost(subredditName, title, content):
+def submitSelfPost(subredditName, title, content, flair_id):
+    subreddit = getSubreddit(subredditName)
+    subreddit.submit(title, selftext=content, flair_id=flair_id)
+
+def getFlairs(subredditName):
+    flairSelect = "r/{0}/api/flairselector/".format(subredditName) #Retrieve the flairs using the Reddit API rather than praw
+    flairs = reddit.post(flairSelect, data={"is_newlink": True})["choices"]
+    return flairs
+
+def getSubreddit(subredditName):
     subreddit = reddit.subreddit(subredditName)
-    subreddit.submit(title, content)
+    return subreddit
