f94837b178eaed9c2ca0e9f44aae730d0e3349e1
Rodin10 <r.jagga@live.nl>
2022-02-04 13:19:47 +0100

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -7,7 +7,7 @@ def getUpdates(telegram_token, offset):
         json_response = json.load(response)
         return json_response #return the json
 
-def forwardMessage(telegram_token, new_chat_id, from_chat_id, message_id):
+def forwardMessage(telegram_token, new_chat_id, from_chat_id, message_id): #Forward messages to specified Telegram groups
     url = "https://api.telegram.org/bot{token}/forwardMessage".format(token = telegram_token)
     myobj = {"from_chat_id" : from_chat_id, "chat_id" : new_chat_id, "message_id" : message_id}
     x = requests.post(url, data = myobj)
